8e2376e6ba158dd5b03c9fe620b139f736723750
KislyKamil <kislykamil@gmail.com>
2020-05-07 21:10:26 +0200

    made minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
-import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:path_provider/path_provider.dart';
-import 'dart:developer';
+import 'package:flutter/services.dart';
 
 import 'dart:async';
 import 'dart:io';
@@ -14,7 +11,6 @@ import 'package:sensorplayer/trackList.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -28,8 +24,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -40,103 +34,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  String path = '/storage/sdcard1/muisc';
   static Directory dir = Directory('/storage/sdcard1/muisc');
+  static List<FileSystemEntity> files = dir.listSync();
+  final List<String> items = [];
+
   bool isPlaying = false;
 
-
   AudioPlayer audioPlayer = AudioPlayer();
-
-  static List<FileSystemEntity> files = dir.listSync();
-  //TESTING
-// String xd = files[0].path;
-
- List<String> items = [];
-
-
-
+  String currentFile;
 
   @override
   initState() {
     super.initState();
+    listInit();
+  }
 
-    files.forEach((f) => items.add(f.path));
+  void listInit() {
+    files.forEach((f) =>
+        items.add(f.path.substring(23).replaceAll(RegExp('([.]mp3)'), '')));
+  }
 
-    //print(stuff.length);
+  setCurrentFile(String item) {
+    this.currentFile = item;
+  }
 
-
+  String getFile() {
+    return currentFile;
   }
 
   Future loadMusic() async {
-    audioPlayer.play('${path}01 Cafe Belga.mp3',
-        isLocal: true, volume: 1.0);
+    //audioPlayer.play('$PATH$currentFile', isLocal: true, volume: 1.0);
 
-      if(!isPlaying) audioPlayer.stop();
-
+    if (!isPlaying) audioPlayer.stop();
   }
 
-  void stopOrResume(){
+  void stopOrResume() {
     setState(() {
-      isPlaying ? isPlaying= false: isPlaying = true;
+      isPlaying ? isPlaying = false : isPlaying = true;
       loadMusic();
     });
   }
 
-
-  //final items = List<String>.generate(10000, (i) => "Item $i");
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Sensor Player '),
-          actions: <Widget>[
-            FlatButton.icon(
-                onPressed: showList,
-                textTheme: ButtonTextTheme.normal,
-                textColor: Colors.white,
-                label: Text('songs'),
-                icon: Icon(Icons.music_note),
-            ),
-          ],
-
-
-        ),
-        /*body:  Center(
-          child: ListView.separated(
-          padding: const EdgeInsets.all(8),
-           itemCount: items.length,
-          itemBuilder: (context, index) {
-               return ListTile(
-                    title: Text('${items[index]}'),
-                onTap: null,
-              ) ;
-
-            },
-               separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.indigo, thickness: 1.5,)
-           )
+      appBar: AppBar(
+        title: Text('Sensor Player ',
+            style: TextStyle(fontFamily: 'PressStart2P', fontSize: 17)),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: showList,
+            textTheme: ButtonTextTheme.normal,
+            textColor: Colors.white,
+            label: Text('songs'),
+            icon: Icon(Icons.music_note),
           ),
-
-         */
-          floatingActionButton: new FloatingActionButton(
-
-          child: isPlaying ? Icon(Icons.pause): Icon(Icons.play_arrow),
-          onPressed:  stopOrResume
+        ],
       ),
-
+      floatingActionButton: new FloatingActionButton(
+          child: isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+          onPressed: stopOrResume),
     );
   }
-  showList(){
+
+  showList() {
     return Navigator.push(context, MaterialPageRoute<void>(
       builder: (BuildContext context) {
         return Scaffold(
           appBar: AppBar(title: Text('Track catalog')),
-          body: TrackList(),
+          body: TrackList(items: items),
         );
       },
     ));
   }
-
 }
-
--- lib/trackList.dart
+++ lib/trackList.dart
@@ -1,38 +1,32 @@
-
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:sensorplayer/main.dart';
+
+import 'package:audioplayers/audioplayers.dart';
 
 class TrackList extends StatelessWidget {
 
-  TrackList({Key key}): super(key: key);
+  TrackList({Key key, @required List<String> items}): super(key: key){
 
-  static Directory dir = Directory('/storage/sdcard1/muisc');
-
-  static List<FileSystemEntity> files = dir.listSync();
-
-  List<String> items = [];
-
-
-
-  void listInit(){
-      files.forEach((f) => items.add(f.path));
+    this.items = items;
   }
+  static Directory dir = Directory('/storage/sdcard1/muisc');
+  List<String> items = [];
 
   @override
   Widget build(BuildContext context) {
-    listInit();
+
+
     return Center(
         child: ListView.separated(
           padding: const EdgeInsets.all(8),
           itemCount: items.length,
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text('${items[index]}'),
-              onTap: null,
+              title: Text('${items[index]}', style: TextStyle(fontFamily: 'FredokaOne')),
+             // onTap: ,
             );
-
           },
           separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.indigo, thickness: 1.5,)
         )
