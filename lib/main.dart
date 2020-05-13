778e0c193df1add364fcd72ab458c1a45be7c535
KislyKamil <kislykamil@gmail.com>
2020-05-13 17:14:56 +0200

    Fixed bug with proximity

--- lib/main.dart
+++ lib/main.dart
@@ -54,12 +54,12 @@ class _MyHomePageState extends State<MyHomePage> {
   bool isOn = true;
   double xAxis, yAxis, zAxis;
   bool isProximity = false;
-
+  int counter = 0;
 
   AudioPlayer audioPlayer = AudioPlayer();
 
   @override
-  initState()  {
+  initState() {
     super.initState();
     listInit();
     audioComplete();
@@ -73,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
     proximityEvents.listen((ProximityEvent event) {
       setState(() {
         isProximity = event.getValue();
-
       });
     });
   }
@@ -107,14 +106,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   loadMusic(int index) async {
     if (items.length > 0) {
-      if (isPlaying && !isProximity) {
+      if (isPlaying) {
         audioPlayer.play('${dir.path}${items[index]}.mp3',
             isLocal: true, volume: 1.0);
       } else {
         audioPlayer.pause();
       }
     } else {
-      if (isPlaying && !isProximity) {
+      if (isPlaying) {
         audioPlayer.play('${phoneDir.path}${itemsPhone[index]}.mp3',
             isLocal: true, volume: 1.0);
       } else {
@@ -178,7 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  allowProximity(){
+  allowProximity() {
     if (isProximity) {
       audioPlayer.setVolume(0);
     } else {
@@ -193,149 +192,151 @@ class _MyHomePageState extends State<MyHomePage> {
     print('POSITION = $position');
     print('STATE = ${audioPlayer.state}');
 
-    if(isOn) {
-
+    if (isOn) {
       allowProximity();
-    }else{
+    } else {
       setState(() {
-        audioPlayer.setVolume(1.0);
+        //audioPlayer.setVolume(1.0);
+        loadMusic(startingPoint);
       });
     }
 
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Sensor Player ',
-              style: TextStyle(fontFamily: 'PressStart2P', fontSize: 17)),
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: showList,
-              textTheme: ButtonTextTheme.normal,
-              textColor: Colors.white,
-              label: Text('songs'),
-              icon: Icon(Icons.music_note),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sensor Player ',
+            style: TextStyle(fontFamily: 'PressStart2P', fontSize: 17)),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: showList,
+            textTheme: ButtonTextTheme.normal,
+            textColor: Colors.white,
+            label: Text('songs'),
+            icon: Icon(Icons.music_note),
+          ),
+        ],
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                  key: null,
+                  onPressed: setSensors,
+                  color: const Color(0xFFe0e0e0),
+                  child: Text(
+                    isOn ? 'Sensors ON' : 'Sensors OFF',
+                    style: TextStyle(
+                        fontSize: 12.0,
+                        color: Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Sriracha"),
+                  )),
+            ]),
+        Row(
+          children: <Widget>[
+            Text('Proximity = $isProximity'),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text('X = $xAxis'),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text('Y = $yAxis'),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text('Z = $zAxis'),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text('${items[startingPoint]}'),
+          ],
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '${position.inMinutes}:${position.inSeconds - position.inMinutes * 60} /',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Sriracha"),
+              ),
+              Text(
+                ' ${duration.inMinutes}:${duration.inSeconds - duration.inMinutes * 60}',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Sriracha"),
+              )
+            ]),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            StepProgressIndicator(
+              direction: Axis.horizontal,
+              fallbackLength: width - 10,
+              totalSteps: (duration.inSeconds > 0) ? duration.inSeconds : 10,
+              currentStep: (position.inSeconds > 0) ? position.inSeconds : 0,
+              size: 5,
+              padding: 0,
+              selectedColor: Colors.redAccent,
+              unselectedColor: Colors.black12,
+              roundedEdges: Radius.circular(10),
             ),
           ],
         ),
-        body: Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                    key: null,
-                    onPressed: setSensors,
-                    color: const Color(0xFFe0e0e0),
-                    child: Text(
-                      isOn ? 'Sensors ON' : 'Sensors OFF',
-                      style: TextStyle(
-                          fontSize: 12.0,
-                          color: Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Sriracha"),
-                    )),
-              ]),
-          Row(
-            children: <Widget>[
-              Text('Proximity = $isProximity'),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text('X = $xAxis'),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text('Y = $yAxis'),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text('Z = $zAxis'),
-            ],
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '${position.inMinutes}:${position.inSeconds - position.inMinutes * 60} /',
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Sriracha"),
-                ),
-                Text(
-                  ' ${duration.inMinutes}:${duration.inSeconds - duration.inMinutes * 60}',
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Sriracha"),
-                )
-              ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              StepProgressIndicator(
-                direction: Axis.horizontal,
-                fallbackLength: width - 10,
-                totalSteps: (duration.inSeconds > 0) ? duration.inSeconds : 10,
-                currentStep: (position.inSeconds > 0) ? position.inSeconds : 0,
-                size: 5,
-                padding: 0,
-                selectedColor: Colors.redAccent,
-                unselectedColor: Colors.black12,
-                roundedEdges: Radius.circular(10),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: ButtonBar(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    FloatingActionButton(
-                      child: Icon(Icons.skip_previous),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: ButtonBar(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(Icons.skip_previous),
+                    backgroundColor: Colors.blueAccent,
+                    onPressed: previousTrack,
+                    heroTag: 'btn1',
+                  ),
+                  FloatingActionButton(
+                      child: playIcon(),
                       backgroundColor: Colors.blueAccent,
-                      onPressed: previousTrack,
-                      heroTag: 'btn1',
-                    ),
-                    FloatingActionButton(
-                        child: playIcon(),
-                        backgroundColor: Colors.blueAccent,
-                        onPressed: stopOrResume,
-                        heroTag: 'btn2'),
-                    FloatingActionButton(
-                        child: Icon(Icons.skip_next),
-                        backgroundColor: Colors.blueAccent,
-                        onPressed: nextTrack,
-                        heroTag: 'btn3'),
-                    FloatingActionButton(
-                      child: Icon(Icons.stop),
-                      backgroundColor: Colors.indigo,
-                      onPressed: stopPlaying,
-                      heroTag: 'btn4',
-                    ),
-                  ],
-                ),
+                      onPressed: stopOrResume,
+                      heroTag: 'btn2'),
+                  FloatingActionButton(
+                      child: Icon(Icons.skip_next),
+                      backgroundColor: Colors.blueAccent,
+                      onPressed: nextTrack,
+                      heroTag: 'btn3'),
+                  FloatingActionButton(
+                    child: Icon(Icons.stop),
+                    backgroundColor: Colors.indigo,
+                    onPressed: stopPlaying,
+                    heroTag: 'btn4',
+                  ),
+                ],
               ),
-            ],
-          ),
-        ]),
-
-      );
-    }
-
-
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
 
   audioDuration() async {
     audioPlayer.onDurationChanged.listen((Duration d) {
@@ -374,9 +375,25 @@ class _MyHomePageState extends State<MyHomePage> {
     sens.accelerometerEvents.listen((sens.AccelerometerEvent event) {
       print(event);
       if (isOn) {
-        if (event.x > 6.0) previousTrack();
+        if (event.x > 5.7 && counter == 0) {
+          previousTrack();
+          setState(() {
+            counter = 1;
+          });
+        }
 
-        if (event.x < -5.4) nextTrack();
+        if (event.x < -5.28 && counter == 0) {
+          nextTrack();
+          setState(() {
+            counter = 1;
+          });
+        }
+
+        if (event.x < 1.3 && event.x > -1.4) {
+          setState(() {
+            counter = 0;
+          });
+        }
       }
       setState(() {
         xAxis = event.x;
